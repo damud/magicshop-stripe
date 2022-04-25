c18bdbe9e6b8fe3e175d31fbd79b6cd070f20c77
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-04-25 19:49:33 +0200

    Fix spacing in FooterBanner.jsx

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -25,15 +25,16 @@ const FooterBanner = ({
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
-          <div className="right"></div>
+        </div>
+        <div className="right">
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
           <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
           </Link>
+          <img src={urlFor(image)} className="footer-banner-image" />
         </div>
-        <img src={urlFor(image)} className="footer-banner-image" />
       </div>
     </div>
   );
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div></div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Magic Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
