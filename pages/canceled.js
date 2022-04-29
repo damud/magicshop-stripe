45e176e18e746f157e045f1f730e3da8386108d9
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-04-29 18:03:05 +0200

    Add cancel page info

--- pages/canceled.js
+++ pages/canceled.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { BsBagCheckFill } from "react-icons/bs";
+import { HiOutlineExclamation } from "react-icons/hi";
 
 import { useStateContext } from "../context/StateContext";
-import { runConfetti } from "../lib/utils";
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
@@ -12,16 +11,14 @@ const Success = () => {
     setCartItems([]);
     setTotalPrice(0);
     setTotalQuantities(0);
-    runConfetti();
   }, []);
   return (
-    <div className="success-wrapper">
-      <div className="success">
+    <div className="cancel-wrapper">
+      <div className="cancel">
         <p className="icon">
-          <BsBagCheckFill />
+          <HiOutlineExclamation size={150} />
         </p>
-        <h2>Thank you for your order!</h2>
-        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <h2>Transaction was canceled by user. Please try again!</h2>
         <p className="description">
           If you have any questions, please send me an email{" "}
           <a href="mailto:order@magicshop.com" className="email">
