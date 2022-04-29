4bff57ae4a5dcfc295f6bb214d94b67f423f35fe
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-04-29 15:54:05 +0200

    Add fireWorks confetti

--- pages/success.js
+++ pages/success.js
@@ -4,7 +4,7 @@ import { BsBagCheckFill } from "react-icons/bs";
 import { useRouter } from "next/router";
 
 import { useStateContext } from "../context/StateContext";
-
+import { runFireworks } from "../lib/utils";
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
   const [order, setOrder] = useState(null);
@@ -14,6 +14,7 @@ const Success = () => {
     setCartItems([]);
     setTotalPrice(0);
     setTotalQuantities(0);
+    runFireworks();
   }, []);
   return (
     <div className="success-wrapper">
